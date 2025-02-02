9245ed8214b32db69cd2412b0cce1a59a4569ced
Gael Lobato <gaelsanchez509@gmail.com>
2025-02-02 14:45:11 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink class="text-3x1 font-bold hover:text-blue-500" to="/">Home</RouterLink>
+        <RouterLink class="text-3x1 font-bold hover:text-blue-500" to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+</template>
