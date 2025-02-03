17ac6f8d48dc8aaebd2fb5a6a76ff71a5e501c12
Gael Lobato <gaelsanchez509@gmail.com>
2025-02-03 17:32:57 -0500

    component with props completed

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Counter from './components/Counter.vue';
 import TodoList from './components/TodoList.vue';
+import ParentComponent from './components/ParentComponent.vue';
 </script>
 
 <template>
 
   <main class="flex justify-center items-center min-h-screen bg-gray-200">
-    <TodoList />
+    <ParentComponent />
   </main>
 
 </template>
